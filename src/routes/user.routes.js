a21286a174965c9e7c4205e082edd10ff14ecc19
raghavgupta000001 <raghav gupta>
2026-02-18 00:39:25 +0530

    Added routes for user

--- src/routes/user.routes.js
+++ src/routes/user.routes.js
@@ -1,5 +1,18 @@
 import { Router } from 'express';
-import { loginUser, logoutUser, registerUser, refreshAccessToken } from '../controllers/user.constroller.js';
+import { 
+    loginUser,
+    logoutUser, 
+    registerUser,
+    refreshAccessToken, 
+    changeCurrentPassword, 
+    getCurrentUser, 
+    updateAccountDetails, 
+    updateUserAvatar, 
+    updateUserCoverImage, 
+    getUserChannelProfile, 
+    getWatchHistory 
+} from '../controllers/user.constroller.js';
+
 import {upload} from '../middlewares/multer.middleware.js'
 import { verifyJWT } from '../middlewares/auth.middleware.js';
 
@@ -36,6 +49,43 @@ router.route('/logout').post(
 
 router.route('/refresh-token').post(
     refreshAccessToken
+);
+
+router.route('/change-password').post(
+    verifyJWT,
+    changeCurrentPassword
+)
+
+router.route('/current-user').post(
+    verifyJWT,
+    getCurrentUser
+)
+
+router.route('/update-account').patch(
+    verifyJWT,
+    updateAccountDetails
+)
+
+router.route('/avatar').patch(
+    verifyJWT,
+    upload.single("avatar"),
+    updateUserAvatar
+)
+
+router.route('/cover-image').patch(
+    verifyJWT,
+    upload.single("coverImage"),
+    updateUserCoverImage
+)
+
+router.route('/c/:username').get(
+    verifyJWT,
+    getUserChannelProfile
+)
+
+router.route('/history').get(
+    verifyJWT,
+    getWatchHistory
 )
 
 export default router;
